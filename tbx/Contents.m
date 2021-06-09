264cfc02c1d95f1c2a2c89a0771c5818cfbb11f6
Georg Drenkhahn <Georg.Drenkhahn@schaeffler.com>
2021-06-09 08:01:13 +0200

    typo

--- tbx/Contents.m
+++ tbx/Contents.m
@@ -2,7 +2,5 @@
 % Version 1.0 1.0 09-Jun-2021
 %
 % Classes:
-%   WarningsNG.Issues - Class to store warning issues in the WarningsNG data format.
+%   WarningsNG.Issue  - Class to store warning issues in the WarningsNG data format.
 %   WarningsNG.Report - Class to collect Issue objects and write them into a XML file.
-
-
