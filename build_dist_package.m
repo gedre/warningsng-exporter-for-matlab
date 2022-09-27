75ab4b0d8719b9373f2f41f7a067365688487b5a
Georg Drenkhahn <Georg.Drenkhahn+github@mailbox.org>
2022-09-27 14:43:47 +0200

    trying to make build script work with GitHub actions

--- build_dist_package.m
+++ build_dist_package.m
@@ -51,8 +51,13 @@ function build_dist_package()
 
     %% Export demo live script as html page
     file_base = fullfile(cfdir, "tbx", "doc", "demo", "WarningsNG_demo");
-    disp("Generating demo html file " + file_base + ".html");
-    demo_file = export(file_base + ".mlx", file_base + ".html");
+    disp("Generating demo .html file");
+    % do not run the live script here.  This is done in the run_demo.m script.
+    % export() was introduced with 2022a
+    demo_file = export(file_base + ".mlx", file_base + ".html", Run=false);
+    disp("... done: " + demo_file);
+    disp("Generating demo .m file");
+    demo_file = export(file_base + ".mlx", file_base + ".m", Run=false);
     disp("... done: " + demo_file);
 
     %% Package
